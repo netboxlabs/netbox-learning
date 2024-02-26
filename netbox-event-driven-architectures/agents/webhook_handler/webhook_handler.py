6c900a69f81beae2bafd8ffcdbe9a80fdeff7e52
Mark Coleman <mcoleman@netboxlabs.com>
2024-02-26 17:13:44 +0000

    Webhook handler now writes to NATs

--- netbox-event-driven-architectures/agents/webhook_handler/webhook_handler.py
+++ netbox-event-driven-architectures/agents/webhook_handler/webhook_handler.py
@@ -1,26 +1,65 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, APIRouter
+import os, asyncio
+from nats.aio.client import Client as NATS
+from dotenv import load_dotenv
 
-app = FastAPI()
+class WebhookHandler():
 
-@app.post('/webhook')
-async def handle_webhook(payload: dict):
-    try:
-        # Process the webhook payload as needed
-        print(f"Received webhook. Data: {payload}")
-        print(f"Event Type: {payload['event']}")
-        print(f"Object Type: {payload['model']}")
+    # Class Variables
+    nats_server = ""
+    publish_subject = ""
+    nc = None
+    router = None
+    app = None
 
-        # Perform actions based on the webhook data
+    # Class Functions
+    def __init__(self):
+        
+        # Load Environment Variables
+        load_dotenv()
+        self.nats_server = os.getenv("NATS_SERVER")
+        self.publish_subject = os.getenv("PUBLISH_SUBJECT")
 
-        # Send a response back to the sender (optional)
-        return {'status': 'success'}
+        # Create a NATS client
+        self.nc = NATS()
 
-    except Exception as e:
-        # Handle exceptions or errors
-        raise HTTPException(status_code=500, detail=str(e))
+        # Create Fast API API Router
+        self.router = APIRouter()
+        self.router.add_api_route("/webhook", self.handle_webhook, methods=["POST"])
+
+        self.app = FastAPI()
+        self.app.include_router(self.router)
+
+        # Report environment
+        print(f"Loaded environment for {os.path.basename(__file__)}. \n NATs Server: {self.nats_server} \n Publishing to subject: {self.publish_subject}")
+
+    async def handle_webhook(self, payload: dict):
+        try:
+            # Process the webhook payload as needed
+            print(f"Received webhook. Data: {payload}")
+            print(f"Event Type: {payload['event']}")
+            print(f"Object Type: {payload['model']}")
+
+            # Perform actions based on the webhook data
+            # Connect to the NATS server
+            await self.nc.connect(self.nats_server)
+
+            # Publish the event
+            print(f"Publishing device fact: {payload}")
+            await self.nc.publish(self.publish_subject, str(payload).encode())
+
+            # Send a response back to the sender (optional)
+            return {'status': 'success'}
+
+        except Exception as e:
+            # Handle exceptions or errors
+            print(f"Exception: {e}")
+            raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == '__main__':
     import uvicorn
 
+    webhook_handler = WebhookHandler()
+
     # Run the FastAPI application using Uvicorn on a specified port (e.g., 5000)
-    uvicorn.run(app, host='0.0.0.0', port=5000)
+    uvicorn.run(webhook_handler.app, host='0.0.0.0', port=5000)
